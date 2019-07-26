1e52c1783a746cb23acf168b119fac5ee9f146a2
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 09:30:45 -0400

    edit seed data to just one route

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-# FoodItem initialization
-hamburger = FoodItem(name="Hamburger", restaurant="McDonalds", dietaryRest=["lactose",])
-hamburger_key = hamburger.put()
-
-# Restaurant initialization
-mcdonalds = Restaurant(name="McDonalds", picture="https://vignette.wikia.nocookie.net/ronaldmcdonald/images/b/b5/Mcdonalds-logo-current-1024x750.png/revision/latest/scale-to-width-down/180?cb=20180730081148",
-                       foodItems=[hamburger_key])
-mcdonalds.put()
-
-
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         login_template = the_jinja_env.get_template('templates/loginpage.html')
@@ -35,9 +25,41 @@ class RestaurantHandler(webapp2.RequestHandler):
         restaurant_template = the_jinja_env.get_template('templates/restaurants.html')
         self.response.write(restaurant_template.render(restaurant_dict))
 
+class PaneraChooseHandler(webapp2.RequestHandler):
+    def get(self):
+        paneraChoose_template = the_jinja_env.get_template('templates/paneraChoose.html')
+        self.response.write(paneraChoose_template.render())
+
+class SeedDatatHandler(webapp2.RequestHandler):
+    def get(self):
+        # FoodItem initialization
+        hamburger = FoodItem(name="Hamburger", restaurant="McDonalds", dietaryRest=["Lactose Intolerant"],)
+        hamburger_key = hamburger.put()
+
+        carnitas = FoodItem(name="Carnitas", restaurant="Chipotle", dietaryRest=["Lactose Intolerant"],)
+        carnitas_key = carnitas.put()
+
+        ciabatta = FoodItem(name="Ciabatta", restaurant="Panera", dietaryRest=["Vegetarian"],)
+        ciabatta_key = ciabatta.put()
+
+        # Restaurant initialization
+        mcdonalds = Restaurant(name="McDonalds", picture="https://vignette.wikia.nocookie.net/ronaldmcdonald/images/b/b5/Mcdonalds-logo-current-1024x750.png/revision/latest/scale-to-width-down/180?cb=20180730081148",
+                               foodItems=[hamburger_key])
+        mcdonalds.put()
+
+        chipotle = Restaurant(name="Chipotle", picture="https://1000logos.net/wp-content/uploads/2017/11/Chipotle-Logo-768x563.png",
+                                foodItems=[carnitas_key])
+        chipotle.put()
+
+        panera = Restaurant(name="Panera", picture="https://1000logos.net/wp-content/uploads/2017/09/Panera-Logo-768x549.png",
+                                foodItems=[ciabatta_key])
+        panera.put()
+        self.response.write("data has been seeded")
 
 
 app = webapp2.WSGIApplication([
     ('/', LoginHandler),
     ('/restaurants', RestaurantHandler),
+    ('/seeddata', SeedDatatHandler),
+    ('/paneraChoose', PaneraChooseHandler),
 ], debug=True)
